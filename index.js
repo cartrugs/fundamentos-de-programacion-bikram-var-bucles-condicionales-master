7bd1ed96e6e1b4c5ec4488bb739c901e23dc9873
cartrugs <cartrugs@gmail.com>
2023-06-17 01:22:59 +0200

    break line deleted

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ let booleanoAnd = booleano1 == booleano2;
 
 let booleanoNot = !booleano1;
 
-let booleanoMix0 =
-  (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
 
 // Operadores
 
